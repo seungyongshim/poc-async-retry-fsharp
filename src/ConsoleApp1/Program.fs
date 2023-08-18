71a5f671af3c07108f3a8f4e01a17b9899c00de6
심승용 <seungyongshim@nexon.co.kr>
2023-08-18 21:33:06 +0900

    .

--- src/ConsoleApp1/Program.fs
+++ src/ConsoleApp1/Program.fs
@@ -1,2 +1,16 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+open AsyncExtensions
+open System
+open FsToolkit.ErrorHandling.AsyncResultCE
+open FsToolkit.ErrorHandling
+
+exception TestError
+
+let x = asyncResult {
+    printfn "Hello from F#"
+    Result.mapError raise TestError
+}
+
+retry 9 x |> Async.RunSynchronously
+
+//retry 9 x |> Async.AwaitTask |> Async.RunSynchronously
+
